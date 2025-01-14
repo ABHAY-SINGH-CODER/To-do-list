bd08094b2a2c024675106f49ecb841ff773976a9
ABHAY-SINGH-CODER <abhaysinghsun@gmail.com>
2025-01-14 21:17:26 +0530

    new version

--- logic1.js
+++ logic1.js
@@ -1,31 +1,30 @@
-/*Author Name:Abhay Singh
-  Dated:21.12.2024
-  Time:30 past 6
-  Objective:To make logic behined working of To do list project*/
+let add_instance = document.getElementById("enter");
+add_instance.addEventListener("click",add_item);
+count = 0;
+let task = [];
+function add_item(){
+   let val = document.getElementById("taskname");
+   let task_name = val.value;
+   let add_task = document.getElementById("task");
+   count++;
+   let n = `t${count}`;
+  
+   add_task.innerHTML += `<div class="box" id="${n}">${task_name}<a class="delete"  onclick="deletet('${n}')">
+   Delete</a></div>`;
+  
+   
 
-let count = 0;  //Calculate Number of objective in list
-
-
-//This add a itemm in the list
-function add_item(){  
-  document.getElementById('list_1').innerHTML ;
-  count = count + 1;
-  document.getElementById('list_1').innerHTML += `<li id="item${count}" ><input type="text" id="text${count}" class="input"><button onclick="change()" class="add" >+</button></li>`;   
- }
-
- //This remove last item from the list
- function remove_item(){
-    let t ="item" + count ;
-    console.log(t);
-    document.getElementById(t).remove() ;
-    count = count - 1;
-
- }
+}
 
 
- //This change text type input to text after user define its objective in list
- function change(){
-    let value = document.getElementById(`text${count}`).value;
-    document.getElementById(`text${count}`).remove();
-    document.getElementById(`item${count}`).innerHTML = `<p>${value}</p>`
- }
+
+function deletet(string1){
+   console.log(string1);
+   let now = document.getElementById(string1);
+   now.style.textDecoration = "line-through";
+   now.style.color ="red";
+   now.style.fontStyle = "italics";
+   
+   
+       
+}
